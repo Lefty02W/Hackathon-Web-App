6b4c866b38a4bb46770d546f21235f1fea7c4a23
jma361 <jma361@uclive.ac.nz>
2019-07-13 14:26:17 +1200

    Added article links and videos to info page

--- vac-app/src/components/Info.vue
+++ vac-app/src/components/Info.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
-    Hello World - Info page
+    <h1>Info Page</h1>
+    <div id = 'articles'>
+      <table>
+        <td>Articles</td>
+        <tr v-for="article in articles">
+          <a v-bind:href="article.link"> <td>{{article.text}}</td> </a>
+        </tr>
+      </table>
+    </div>
+    <div>
+      <div>
+        <youtube :video-id="videoId"></youtube>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import {getIdFromURL} from 'vue-youtube-embed'
 export default {
   name: 'Info',
   data () {
     return {
-      msg: 'Stuff'
+      articles: [
+        {text: 'Article One', link: 'http://www.google.com'},
+        {text: 'Article Two', link: 'http://www.google.com'},
+        {text: 'Article Three', link: 'http://www.google.com'},
+        {text: 'Article Four', link: 'http://www.google.com'}
+      ],
+      videoId: getIdFromURL('https://www.youtube.com/watch?v=lCDCOckaTTg')
     }
   }
 }
--- vac-app/src/router/index.js
+++ vac-app/src/router/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Info from '@/components/Info.vue'
 import Stats from '@/components/Stats.vue'
-
+import VueYouTubeEmbed from 'vue-youtube-embed'
+Vue.use(VueYouTubeEmbed)
 
 Vue.use(Router)
 
